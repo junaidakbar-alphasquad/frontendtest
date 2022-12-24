aa44ef707416419391a6b230e50754887e8c0d7f
junaidakbar-alphasquad <junaidakbar.alphasquad@gmail.com>
2022-12-24 17:14:06 +0500

    test

--- components/Pagelayout/Header.js
+++ components/Pagelayout/Header.js
@@ -1,21 +1,27 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const Header = () => {
-  return (
-    <div className='w-auto bg-red-400 right-0 left-[200px] top-0 fixed'>
-<nav className='flex justify-between w-full'>
-    <div>button1
-            </div>
-    <div>button2
-            </div>
-    <div>button3
-            </div>
-    <div>button4
-            </div>
-</nav>
+    const router = useRouter()
+    const menubuttons = [
+        { name: 'Posts', id: '1', link: '/posts' },
+        { name: 'Users', id: '2', link: '/users' },
+        { name: 'Login', id: '3', link: '/login' },
+        { name: 'Sign Up', id: '4', link: '/register' },
+    ]
+    return (
+        <div className='w-auto bg-red-400 p-2 right-0 left-[200px] top-0 fixed'>
+            <nav className='flex justify-between w-full'>
+                {menubuttons.map((menu, ind) => (
+                    <div onClick={() => router.push(menu.link)} key={menu.id} className={`${router.asPath == menu.link ? 'bg-slate-400' : 'bg-slate-200'}`}>
+                        {menu.name}
 
-    </div>
-  )
+                    </div>
+                ))}
+            </nav>
+
+        </div>
+    )
 }
 
 export default Header
--- components/Pagelayout/Sidebar.js
+++ components/Pagelayout/Sidebar.js
@@ -1,11 +1,28 @@
 import React from 'react'
 
 const Sidebar = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    const menubuttons = [
+        { name: 'menu1', id: 1 },
+        { name: 'menu2', id: 2 },
+        { name: 'menu3', id: 3 },
+        { name: 'menu4', id: 4 },
+    ]
+    return (
+        <div>
+            <div className='w-full h-10 pt-2 flex justify-center'>image logo</div>
+            <ul className='flex flex-col gap-2'>
+                {
+                    menubuttons.map((menu, ind) => (
+
+                        <li key={menu.id} className={'w-full rounded-lg px-3 py-2 bg-slate-400'}>
+                            {menu.name}
+
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default Sidebar
